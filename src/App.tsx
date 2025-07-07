427e40d8ab26c61c783d8295a7056f36e310fe5b
Suraj Giwa <giwas@uwindsor.ca>
2025-07-07 00:31:27 -0400

    installing amplify

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-
 import './App.css'
-
 function App() {
   const [count, setCount] = useState(0)
 
@@ -10,7 +8,7 @@ function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          
+        
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
@@ -19,7 +17,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count is  {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
